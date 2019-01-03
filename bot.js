8895c3e2aaeee1bc0fa452d5734c2197b01111d4
My Home <hungluongduc@vccorp.vn>
2019-01-04 01:11:02 +0700

    địt con mẹ minh béo

--- bot.js
+++ bot.js
@@ -3,35 +3,34 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-client.on('ready', () = > {
+
+client.on('ready', () => {
 
     console.log('I am ready!');
 
-})
-;
+});
 
 
-client.on('message', message = > {
-    if (message.content === 'sủa đi minh'
-)
-{
+
+client.on('message', message => {
+    if (message.content === 'sủa đi minh') {
 
     message.reply('gâu gâu');
 
 }
-client.on('guildMemberAdd', member = > {
+
+});
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
     // Send the message to a designated channel on a server:
-    const channel = member.guild.channels.find(ch = > ch.name === 'general'
-)
-;
+    const channel = member.guild.channels.find(ch => ch.name === 'member-log');
 // Do nothing if the channel wasn't found on this server
 if (!channel) return;
 // Send the message, mentioning the member
 channel.send(`Chào bé đến với SBTC nhaaa, ${member}, mãi yêu!`);
-})
-;
-})
-;
+});
+
 
 
 // THIS  MUST  BE  THIS  WAY
