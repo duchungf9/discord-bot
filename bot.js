58d24bc7919ecf01fdddd6bc17ed4f1c69359438
hungluongduc <hungluongduc@sohagame.vn>
2019-01-04 14:52:03 +0700

    123

--- bot.js
+++ bot.js
@@ -15,19 +15,15 @@ client.on('message', message => {
     if (message.content === 'sủa đi minh') {
     const exampleEmbed = new Discord.RichEmbed()
         .setColor('#0099ff')
-        .setTitle('Some title')
+        .setTitle('Em là Minh béo')
         .setURL('https://discord.js.org/')
-        .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-        .setDescription('Some description here')
-        .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-        .addField('Regular field title', 'Some value here')
-        .addBlankField()
-        .addField('Inline field title', 'Some value here', true)
-        .addField('Inline field title', 'Some value here', true)
-        .addField('Inline field title', 'Some value here', true)
-        .setImage('https://i.imgur.com/wSTFkRM.png')
-        .setTimestamp()
-        .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+        .setAuthor('Minh béo', 'http://sohanews.sohacdn.com/thumb_w/660/2017/photo1499416230355-1499416230506-0-0-262-421-crop-1499416275290.jpg', 'https://xvideos.com')
+        .setDescription('Gâu gâu gâu!')
+        .setThumbnail('http://sohanews.sohacdn.com/thumb_w/660/2017/photo1499416230355-1499416230506-0-0-262-421-crop-1499416275290.jpg')
+
+        .setImage('http://lifecompass.vn/wp-content/uploads/2015/03/cho-sua.jpg')
+        .setTimestamp();
+
     message.channel.send({embed:exampleEmbed});
 
 
