847a28e4cfecda835355156ffdb927e514916ad3
hungluongduc <hungluongduc@sohagame.vn>
2019-01-04 16:10:10 +0700

    123

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ client.on('message', message => {
 
 
 }
-    var random  = Math.floor(Math.random() * 7);
+    var random  = Math.floor(Math.random() * 8);
     var So = Math.floor(Math.random() * 100);
     console.log(random);
     if(random==7){
