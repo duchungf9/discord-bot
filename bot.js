30bd767a76e65c6ab7f5c9631d07c90749105eb5
hungluongduc <hungluongduc@sohagame.vn>
2019-01-04 11:51:40 +0700

    123

--- bot.js
+++ bot.js
@@ -1,31 +1,38 @@
 const Discord = require('discord.js');
 
-var client = new Discord.Client();
+const client = new Discord.Client();
 
 console.log('địt');
 console.log(process.env.BOT_TOKEN);
 console.log('-------');
 
-client.on('ready', () = > {
+client.on('ready', () => {
+
     console.log('I am ready!');
+
 });
 
 
-client.on('message', message = > {
-    if(message.content === 'sủa đi minh'){
-        message.reply('gâu gâu');
-    }
+
+client.on('message', message => {
+    if (message.content === 'sủa đi minh') {
+
+    message.reply('gâu gâu');
+
+}
+
 });
 
 // Create an event listener for new guild members
-client.on('guildMemberAdd', member = > {
+client.on('guildMemberAdd', member => {
     // Send the message to a designated channel on a server:
-    const channel = member.guild.channels.find(ch = > ch.name === 'member-log');
-    // Do nothing if the channel wasn't found on this server
-    if (!channel) return;
-    // Send the message, mentioning the member
-    channel.send(`Chào bé đến với SBTC nhaaa, ${member}, mãi yêu!`);
+    const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+// Do nothing if the channel wasn't found on this server
+if (!channel) return;
+// Send the message, mentioning the member
+channel.send(`Chào bé đến với SBTC nhaaa, ${member}, mãi yêu!`);
 });
+
 
 
 // THIS  MUST  BE  THIS  WAY
