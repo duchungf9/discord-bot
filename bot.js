7d08f80667c6b0f0e75e7b227ca97ed925594df2
hungluongduc <hungluongduc@sohagame.vn>
2019-01-04 11:30:32 +0700

    123

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+var money = require('discord-money');
 
 const client = new Discord.Client();
 
@@ -6,33 +7,32 @@ console.log('địt');
 console.log(process.env.BOT_TOKEN);
 console.log('-------');
 
-client.on('ready', () => {
-
+client.on('ready', () = > {
     console.log('I am ready!');
-
 });
 
 
-
-client.on('message', message => {
-    if (message.content === 'sủa đi minh') {
-
-    message.reply('gâu gâu');
-
-}
-
+client.on('message', message = > {
+    let prefix = ".";
+    if (message.content.toUpperCase() === `${prefix}BALANCE`) {
+            money.fetchBal(message.author.id).then((i) => { // money.fetchBal grabs the userID, finds it, and puts it into 'i'.
+                message.channel.send(`**Balance:** ${i.money}`);
+            })
+    }
+    if(message.content === 'sủa đi minh'){
+        message.reply('gâu gâu');
+    }
 });
 
 // Create an event listener for new guild members
-client.on('guildMemberAdd', member => {
+client.on('guildMemberAdd', member = > {
     // Send the message to a designated channel on a server:
-    const channel = member.guild.channels.find(ch => ch.name === 'member-log');
-// Do nothing if the channel wasn't found on this server
-if (!channel) return;
-// Send the message, mentioning the member
-channel.send(`Chào bé đến với SBTC nhaaa, ${member}, mãi yêu!`);
+    const channel = member.guild.channels.find(ch = > ch.name === 'member-log');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`Chào bé đến với SBTC nhaaa, ${member}, mãi yêu!`);
 });
-
 
 
 // THIS  MUST  BE  THIS  WAY
