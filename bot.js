cc9fec523ab1a57cc5036f4b4550931cc41b2fe5
hungluongduc <hungluongduc@sohagame.vn>
2019-01-05 17:20:05 +0700

    123

--- bot.js
+++ bot.js
@@ -75,7 +75,7 @@ client.on('message', message => {
         });
 
     }
-    var random  = Math.floor(Math.random() * 50);
+    var random  = Math.floor(Math.random() * 100);
     var So = Math.floor(Math.random() * 20);
     console.log(random);
     var soCurrency = "2ST4m28";
