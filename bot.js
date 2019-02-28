243e4ca45b2d8227d1b032e2806a637f6e7eb862
hungluongduc <hungluongduc@sohagame.vn>
2019-02-28 15:43:28 +0700

    123

--- bot.js
+++ bot.js
@@ -170,8 +170,8 @@ function minhbeosua(message){
 }
 function getCurrentCurrency(usersRef) {
     return _user = usersRef.once('value', function (snapshot) {
-        if(typeof snapshot.val() != 'undefined'){
-            console.log(snapshot.val().sip);
+        if(typeof snapshot.val().sip != 'undefined'){
+            // console.log(snapshot.val().sip);
             var siphientai = 0;
             if (snapshot.exists()) {
                 siphientai = snapshot.val().sip;
